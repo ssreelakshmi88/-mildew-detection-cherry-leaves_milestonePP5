3bc105e57c25d974a2daff5d36e23924a65ca278
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2023-02-14 20:39:43 +0000

    Created app.py file

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from app_pages.multipage import MultiPage
+
+# load pages scripts
+
+from app_pages.page_project_summary import page_project_summary_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_leaves_visualizer import page_leaves_visualizer_body
+from app_pages.page_ml_performance import page_ml_performance_body
+from app_pages.page_mildew_detector import page_mildew_detector_body
+
+
+app = MultiPage(app_name="Cherry Leaves Mildew Detection")
+
+
+app.add_page("Project Summary", page_project_summary_body)
+app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
+app.add_page("Cherry Leaf Visualizer", page_leaves_visualizer_body)
+app.add_page('Mildew Detector', page_mildew_detector_body)
+app.add_page('ML Performance Metric', page_ml_performance_body)
+
+app.run()
