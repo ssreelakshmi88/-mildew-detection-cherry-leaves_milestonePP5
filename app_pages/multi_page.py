8a65d34732ec1a71f3ffcb6465920ebfa6d3fd28
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2023-02-15 20:43:09 +0000

    Create multipage.py file

---
+++ app_pages/multi_page.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+class MultiPage:
+    """ This class generates multiple Streamlit pages
+    """
+
+    def __init__(self, app_name) -> None:
+        self.pages = []
+        self.app_name = app_name
+
+        st.set_page_config(
+            page_title=self.app_name,
+        )
+
+    def add_page(self, title, func) -> None:
+        """ Adds title"""
+        self.pages.append({"title": title, "function": func})
+
+    def run(self):
+        """Add title and menu names"""
+        st.title(self.app_name)
+        page = st.sidebar.radio('Menu', self.pages,
+                                format_func=lambda page: page['title'])
+        page['function']()
