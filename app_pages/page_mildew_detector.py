dc7e0a990278d3e9a7e2487a9854e125e94e4880
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2023-02-19 14:24:00 +0000

    Add functionality to make live predictions

--- app_pages/page_mildew_detector.py
+++ app_pages/page_mildew_detector.py
@@ -41,3 +41,35 @@ def page_mildew_detector_body():
 
     if predict_button:
         make_live_predict(images_buffer)
+
+
+def make_live_predict(images_buffer):
+    """
+    This function uploads images and make live predictions
+
+    """
+
+    if images_buffer is not None:
+        df_report = pd.DataFrame([])
+        for image in images_buffer:
+
+            img_pil = (Image.open(image))
+            st.info(f"Cherry leaves Sample: **{image.name}**")
+            img_array = np.array(img_pil)
+            st.image(
+                img_pil, caption=f"Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
+
+            version = 'v1'
+            resized_img = resize_input_image(img=img_pil, version=version)
+            pred_proba, pred_class = load_model_and_predict(
+                resized_img, version=version)
+            plot_predictions_probabilities(pred_proba, pred_class)
+
+            df_report = df_report.append({"Name": image.name, 'Result': pred_class},
+                                         ignore_index=True)
+
+        if not df_report.empty:
+            st.success("Analysis Report")
+            st.table(df_report)
+            st.markdown(download_dataframe_as_csv(
+                df_report), unsafe_allow_html=True)
