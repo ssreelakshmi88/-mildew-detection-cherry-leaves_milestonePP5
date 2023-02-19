ad3e96cc1160c041a21a4a7f23d23a4ff5fcebd2
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2023-02-19 20:55:23 +0000

    Modify code in leaves_visualizer.py

--- app_pages/page_leaves_visualizer.py
+++ app_pages/page_leaves_visualizer.py
@@ -75,7 +75,7 @@ def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15, 10)):
     labels = os.listdir(dir_path)
 
     # checks if your montage space is greater than subset size
-    images_list = os.listdir(dir_path+'/' + label_to_display)
+    images_list = os.listdir(dir_path + '/' + label_to_display)
     if nrows * ncols < len(images_list):
         img_idx = random.sample(images_list, nrows * ncols)
 
